3604b436ec5ca77b5ed1d6286ab9b90ab7cbaa69
XionN <maditya919.ma@gmail.com>
2024-05-26 18:57:23 +0700

    Revisi Landing Page(2)

--- application/views/index.php
+++ application/views/index.php
@@ -1,62 +1,94 @@
 <!-- jumbotron -->
 <div class="jumbotron jumbotron-fluid index-jumbotron rellax" data-rellax-speed="-2">
-    <div class="container">
-        <p class="lead text-jumbotron" data-aos="fade-right" data-aos-duration="1700">Selamat Datang di</p>
-        <h1 class="display-3 header-jumbotron"  data-aos="fade-right" data-aos-duration="1500">CodeChamp
-        </h1>
-        <p class="lead text-jumbotron" data-aos="fWde-right" data-aos-duration="1700">Layanan Kursus Online Berbahasa Indonesia</p>
-        <a href="https://github.com/siubsi" target="_blank" title="Kunjungi GitHub SIUBSI"><i class="lni lni-github-original lni-custom-style" data-aos="fade-right" data-aos-duration="1700"></i></a>
-        <a href="<?= base_url('#materikursus'); ?>" title="Lihat Pembuat Forum"><i class="fa fa-book fa-custom-style" style="margin-left: 10px;" data-aos="fade-right" data-aos-duration="1700"></i></a>
+  <div class="container">
+    <h1 class="display-3 header-jumbotron" data-aos="fade-right" data-aos-duration="1700">CodeChamp
+    </h1>
+    <p class="lead text-jumbotron" data-aos="fade-right" data-aos-duration="1700">Layanan Kursus Online Berbahasa Indonesia</p>
+    <div class="center-container">
+      <a href="https://github.com/siubsi" target="_blank" title="Kunjungi GitHub SIUBSI"><i class="lni lni-github-original lni-custom-style" data-aos="fade-right" data-aos-duration="1700"></i></a>
+      <a href="<?= base_url('#materikursus'); ?>" title="Lihat Pembuat Forum"><i class="fa fa-book fa-custom-style" style="margin-left: 10px;" data-aos="fade-right" data-aos-duration="1700"></i></a>
     </div>
+  </div>
 </div>
 <!-- end jumbotron -->
 <br><br><br>
 
 <!-- title -->
-<div class="row" id="tentang">
-    <div class="col-md-10 container mt-5">
-        <h2 class="title text-center" data-aos="fade-down" data-aos-duration="1500">Mengenai CodeChamp</h2>
+<div class="row" id="materikursus">
+  <div class="col-md-10 container mt-5">
+    <h2 class="title text-center" data-aos="fade-down" data-aos-duration="1500">Materi Kursus Tersedia</h2>
+  </div>
+</div>
+<br>
+
+<div class="row">
+  <div class="owl-carousel mt-3 container owl-theme">
+    <div class="b-game-card item" data-aos="fade-up" data-aos-duration="1200">
+      <div class="b-game-card__cover" style="background-image: url(assets/dist/img/html.png);"></div>
     </div>
+    <div class="b-game-card item" data-aos="fade-up" data-aos-duration="1200">
+      <div class="b-game-card__cover" style="background-image: url(assets/dist/img/css.png);"></div>
+    </div>
+    <div class="b-game-card item" data-aos="fade-up" data-aos-duration="1200">
+      <div class="b-game-card__cover" style="background-image: url(assets/dist/img/javascript.png);"></div>
+    </div>
+    <div class="b-game-card item" data-aos="fade-up" data-aos-duration="1200">
+      <div class="b-game-card__cover" style="background-image: url(assets/dist/img/git.png);"></div>
+    </div>
+    <div class="b-game-card item" data-aos="fade-up" data-aos-duration="1200">
+      <div class="b-game-card__cover" style="background-image: url(assets/dist/img/php.png);"></div>
+    </div>
+    <div class="b-game-card item" data-aos="fade-up" data-aos-duration="1200">
+      <div class="b-game-card__cover" style="background-image: url(assets/dist/img/bootstrap.png);"></div>
+    </div>
+  </div>
+</div>
+
+<!-- title -->
+<div class="row" id="tentang">
+  <div class="col-md-10 container mt-5">
+    <h2 class="title text-center" data-aos="fade-down" data-aos-duration="1500">Mengenai CodeChamp</h2>
+  </div>
 </div>
 <br>
 
 <section class="wrapper">
-<div class="container">
+  <div class="container">
     <div class="row mt-3">
-        <div class="col-md-6" data-aos-duration="1500" data-aos="fade-right">
-            <img src="<?= base_url('assets/') ?>dist/img/vestiazeta5.png" style="border: 2px solid #7743DB; border-radius: 20px;" class="img-fluid" alt="" srcset="">
-        </div>
-        <div class="col-md-6 my-auto" data-aos-duration="1500" data-aos="fade-left">
-            <h1 class="title mt-3">Apa itu CodeChamp</h1>
-            <p class="text-section"><span style="font-size: 18px; color: #7743DB;">CodeChamp adalah Layanan Kursus Online Berbahasa Indonesia.</span><br>Materi kursus yang kami sediakan berfokuskan pada Web Development, jika anda berminat untuk mempelajari mengenai Web Development. Daftar sekarang juga secara gratis!</p>
-            <a href="<?= base_url('auth/register') ?>"><button class="btn btn-daftar">Daftarkan akun sekarang juga!</button></a>
-        </div>
+      <div class="col-md-6" data-aos-duration="1500" data-aos="fade-right">
+        <img src="<?= base_url('assets/') ?>dist/img/coding.png" style="border: 2px solid #7743DB; border-radius: 20px;" class="img-fluid" alt="" srcset="">
+      </div>
+      <div class="col-md-6 my-auto" data-aos-duration="1500" data-aos="fade-left">
+        <h1 class="title mt-3">Apa itu CodeChamp ?</h1>
+        <p class="text-section"><span style="font-size: 18px; color: #7743DB;">CodeChamp adalah Layanan Kursus Online Berbahasa Indonesia.</span><br>Materi kursus yang kami sediakan berfokuskan pada Web Development, jika anda berminat untuk mempelajari mengenai Web Development. Daftar sekarang juga secara gratis!</p>
+        <a href="<?= base_url('auth/register') ?>"><button class="btn btn-daftar">Daftarkan akun sekarang juga!</button></a>
+      </div>
     </div>
-</div>
+  </div>
 </section>
 
 <!-- title -->
 <div class="row" id="daftarfitur">
-    <div class="col-md-10 container mt-5">
-        <h2 class="title text-center" data-aos="fade-down" data-aos-duration="1500">Apa saja yang bisa dilakukan di CodeChamp</h2>
-    </div>
+  <div class="col-md-10 container mt-5">
+    <h2 class="title text-center" data-aos="fade-down" data-aos-duration="1500">Apa saja yang bisa dilakukan di CodeChamp</h2>
+  </div>
 </div>
-<br>    
+<br>
 
 <section class="wrapper">
   <div class="container">
     <div class="row mt-3">
       <div class="col-sm-12 col-md-6 col-lg-4 mb-4">
         <div class="card text-light card-has-bg click-col" data-aos="fade-up" data-aos-duration="1500">
-            <div class="card-img-overlay d-flex flex-column">
-                <div class="card-body">
-                    <i class="fa fa-book fa-custom-style"></i>
-                    <h4 class="card-title mt-0">Terdapat Informasi Materi</h4>
-                    <small class="text-light">Pengguna disediakan informasi mengenai banyak materi web development, seperti misalnya HTML, CSS, Javascript, dan lain-lain.</small>
+          <div class="card-img-overlay d-flex flex-column">
+            <div class="card-body">
+              <i class="fa fa-book fa-custom-style"></i>
+              <h4 class="card-title mt-0">Terdapat Informasi Materi</h4>
+              <small class="text-light">Pengguna disediakan informasi mengenai banyak materi web development, seperti misalnya HTML, CSS, Javascript, dan lain-lain.</small>
             </div>
             <div class="card-footer">
               <div class="media">
-                <img class="mr-3" src="<?= base_url('assets/') ?>dist/img/codechamplogo.png" width="40" alt="Codechamp Logo" >
+                <img class="mr-3" src="<?= base_url('assets/') ?>dist/img/codechamplogo.png" width="40" alt="Codechamp Logo">
                 <div class="media-body">
                   <h6 class="my-0 text-light d-block">CodeChamp</h6>
                   <small class="text-light">Layanan Kursus Online</small>
@@ -76,7 +108,7 @@
             </div>
             <div class="card-footer">
               <div class="media">
-                <img class="mr-3" src="<?= base_url('assets/') ?>dist/img/codechamplogo.png" width="40" alt="Codechamp Logo" >
+                <img class="mr-3" src="<?= base_url('assets/') ?>dist/img/codechamplogo.png" width="40" alt="Codechamp Logo">
                 <div class="media-body">
                   <h6 class="my-0 text-light d-block">CodeChamp</h6>
                   <small class="text-light">Layanan Kursus Online</small>
@@ -96,7 +128,7 @@
             </div>
             <div class="card-footer">
               <div class="media">
-                <img class="mr-3" src="<?= base_url('assets/') ?>dist/img/codechamplogo.png" width="40" alt="Codechamp Logo" >
+                <img class="mr-3" src="<?= base_url('assets/') ?>dist/img/codechamplogo.png" width="40" alt="Codechamp Logo">
                 <div class="media-body">
                   <h6 class="my-0 text-light d-block">CodeChamp</h6>
                   <small class="text-light">Layanan Kursus Online</small>
@@ -113,40 +145,10 @@
 
 </div>
 
-<!-- title -->
-<div class="row" id="materikursus">
-    <div class="col-md-10 container mt-5">
-        <h2 class="title text-center" data-aos="fade-down" data-aos-duration="1500">Materi Kursus Tersedia</h2>
-    </div>
-</div>
-<br>
-
-<div class="row">
-    <div class="owl-carousel mt-3 container owl-theme">
-        <div class="b-game-card item" data-aos="fade-up" data-aos-duration="1200">
-            <div class="b-game-card__cover" style="background-image: url(assets/dist/img/html.png);"></div>
-        </div>
-        <div class="b-game-card item" data-aos="fade-up" data-aos-duration="1200">
-            <div class="b-game-card__cover" style="background-image: url(assets/dist/img/css.png);"></div>
-        </div>
-        <div class="b-game-card item" data-aos="fade-up" data-aos-duration="1200">
-            <div class="b-game-card__cover" style="background-image: url(assets/dist/img/javascript.png);"></div>
-        </div>
-        <div class="b-game-card item" data-aos="fade-up" data-aos-duration="1200">
-            <div class="b-game-card__cover" style="background-image: url(assets/dist/img/git.png);"></div>
-        </div>
-        <div class="b-game-card item" data-aos="fade-up" data-aos-duration="1200">
-            <div class="b-game-card__cover" style="background-image: url(assets/dist/img/php.png);"></div>
-        </div>
-        <div class="b-game-card item" data-aos="fade-up" data-aos-duration="1200">
-            <div class="b-game-card__cover" style="background-image: url(assets/dist/img/bootstrap.png);"></div>
-        </div>
-    </div>
-</div>
 
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/rellax/1.12.1/rellax.min.js"></script>
 
 <script>
-    var rellax = new Rellax('.rellax');
+  var rellax = new Rellax('.rellax');
 </script>
